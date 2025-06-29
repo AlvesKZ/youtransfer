fefb55d9f3bede89e900f0690d755127ce36f7df
AlvesKZ <igoralvex01@gmail.com>
2025-06-29 01:52:42 -0300

    chore: configure the project

---
+++ src/main/java/com/alves/youtransfer/YoutransferApplication.java
@@ -0,0 +1,13 @@
+package com.alves.youtransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutransferApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alves/youtransfer/YoutransferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alves.youtransfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutransferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
